c4bac60eec360754e9c2a95cf68b3c6ec88e19c9
gabrielgusso <gabriel.gusso@hotmail.com>
2022-09-19 11:30:27 -0300

    User component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,29 +1,36 @@
 import { useState } from "react";
 import Sugestoes from "./Sugestoes";
 
-export default function Sidebar() {
+function UserName(){
   const [nome, setNome] = useState("Catana");
   const [img, setImg] = useState("img-videos/catanacomics1.png");
+  
+  return(
+    <div class="sidebarTop">
+    <img
+      src={img}
+      alt="profile"
+      onClick={() => setImg(prompt("Coloque o link da sua foto aqui"))}
+    />
+    <div sidebarTop-text>
+      <p class="sidebarTop-p1">catanacomics</p>
+      <div class="pencil">
+        <p class="sidebarTop-p2">{nome}</p>
+        <ion-icon
+          name="pencil"
+          onClick={() => setNome(prompt("Qual seu nome?"))}
+        ></ion-icon>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default function Sidebar() {
 
   return (
     <div class="sidebar">
-      <div class="sidebarTop">
-        <img
-          src={img}
-          alt="profile"
-          onClick={() => setImg(prompt("Coloque o link da sua foto aqui"))}
-        />
-        <div sidebarTop-text>
-          <p class="sidebarTop-p1">catanacomics</p>
-          <div class="pencil">
-            <p class="sidebarTop-p2">{nome}</p>
-            <ion-icon
-              name="pencil"
-              onClick={() => setNome(prompt("Qual seu nome?"))}
-            ></ion-icon>
-          </div>
-        </div>
-      </div>
+      <UserName />
       <Sugestoes />
       <p class="aboutUs">
         Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos •
